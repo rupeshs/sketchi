8fb3c572fcaf77a40e7b2e423b4cb2b748b8e8b9
Rupesh Sreeraman <exmplayer.dev@gmail.com>
2017-07-24 22:45:45 +0530

    style stat

--- js/main.js
+++ js/main.js
@@ -191,6 +191,7 @@ var dropAreaElement = document.querySelector('.main');
         ctxt.drawImage(imageTmp,0,0,resultWidth,resultHeight);
         image.src= scanvas.toDataURL();
         delete imageTmp;
+         $("#stat").fadeIn();
         dismsg.innerHTML="Loading photo...";
         image.onload=function(){
            // delete previous images
@@ -422,54 +423,12 @@ for (var dpts in optimizedPoints) {
       //alert( document.querySelector(scanvas.width));
        document.querySelector('.container img').style.opacity = 0;
       document.querySelector('.container svg').style.opacity = 1;
-    dismsg.innerHTML="";
+      $("#stat").fadeOut();
+  
       NProgress.done();
     }, 2500);
   }
 
   });
 
-  function canvasToImage(backgroundColor)
-{
-	//cache height and width		
-	var w = canvas.width;
-	var h = canvas.height;
- 
-	var data;
- 
-	if(backgroundColor)
-	{
-		//get the current ImageData for the canvas.
-		data = context.getImageData(0, 0, w, h);
- 
-		//store the current globalCompositeOperation
-		var compositeOperation = context.globalCompositeOperation;
- 
-		//set to draw behind current content
-		context.globalCompositeOperation = "destination-over";
- 
-		//set background color
-		context.fillStyle = backgroundColor;
- 
-		//draw background / rect on entire canvas
-		context.fillRect(0,0,w,h);
-	}
- 
-	//get the image data from the canvas
-	var imageData = this.canvas.toDataURL("image/png");
- 
-	if(backgroundColor)
-	{
-		//clear the canvas
-		context.clearRect (0,0,w,h);
- 
-		//restore it with original / cached ImageData
-		context.putImageData(data, 0,0);
- 
-		//reset the globalCompositeOperation to what it was
-		context.globalCompositeOperation = compositeOperation;
-	}
- 
-	//return the Base64 encoded data url string
-	return imageData;
-}
+  
