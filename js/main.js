bcdda75187d59d381621f06e8d335e5904d691ce
Rupesh Sreeraman <exmplayer.dev@gmail.com>
2017-07-28 06:47:30 +0530

    ui improv

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
-
 "use strict";
 var dismsg;
 var image;
 var gimage;
- var ctx;
+var ctx;
 var contourFinder;
 var startTime = 0;
 var resultWidth;
@@ -11,14 +10,7 @@ var resultHeight;
 var imageWidth;
 var scanvas;
 var filters, canny, canvas;
-
-$(document).ready(function () {
-
-  NProgress.configure({ showSpinner: false });
-  NProgress.configure({ trickle: false });
-  dismsg = document.getElementById("stat");
-
-  var settings = {
+var settings = {
     Style: 'sketch',
     BrushSize: 1.0,
     Smoothness:1,
@@ -39,129 +31,9 @@ $(document).ready(function () {
     SAME: 8
   };
   var ctx2;
-  var selColorR = 0;
-  var selColorG = 0;
-  var selColorB = 0;
+
   var count = 1;
-
-  var gui = new dat.gui.GUI();
- 
-  gui.add(settings, 'Style', ['sketch', 'simple', 'chalkboard']).onChange(function (style) {
-    
-    if (style == 'chalkboard') {
-      settings.BackgroundColor = [50, 50, 50];
-      settings.BrushColor = [200, 200, 200];
-    }
-    else {
-      settings.BackgroundColor = [255, 255, 255];
-      settings.BrushColor = [50, 50, 50];
-
-    }
-
-  });
- 
-  gui.add(settings, 'BrushSize').min(0).max(5).step(1);
-  gui.add(settings, 'Smoothness').min(1).max(5).step(1);
-  gui.add(settings, 'SketchStrength').min(1).max(100).step(1);
-  
-  var f1 = gui.addFolder('Colors');
-  f1.addColor(settings, 'BrushColor').listen();;
-  f1.addColor(settings, 'BackgroundColor').listen();;
-
-  function downloadCanvas(link, sourceCanvas, filename) {
-    link.href = sourceCanvas.toDataURL();
-    link.download = filename;
-  }
-
-  document.getElementById('download').addEventListener('click', function () {
-    downloadCanvas(this, scanvas, 'sketch.png');
-  }, false);
-  
-  document.getElementById('redo').addEventListener('click', function () {
-    sketchi();
-  }, false);
-
-
-  function sketchi() {
-    
-    var ctxt;
-    var imageTmp = new Image();
-    imageTmp.id = 'result';
-    imageTmp.src = gimage.src;
-    imageTmp.onload = function () {
-
-      var aspectRatio = imageTmp.width / imageTmp.height;
-      console.log("w x h " + imageTmp.width + " " + imageTmp.height);
-      console.log("AR :" + aspectRatio);
-      if (imageTmp.width > 640) {
-        resultWidth = 640;
-        resultHeight = 640 / aspectRatio;
-      }
-      else {
-        resultWidth = imageTmp.width;
-        resultHeight = imageTmp.height;
-
-      }
-
-      console.log("w x h" + resultWidth + " " + resultHeight);
-
-      scanvas = document.createElement('canvas');
-      scanvas.id = 'canvas2';
-      scanvas.width = resultWidth;
-      scanvas.height = resultHeight;
-      ctxt = scanvas.getContext("2d");
-      ctxt.drawImage(imageTmp, 0, 0, resultWidth, resultHeight);
-      gimage.src = scanvas.toDataURL();
-      //delete imageTmp;
-      $("#stat").fadeIn();
-      dismsg.innerHTML = "Loading photo...";
-      gimage.onload = function () {
-        // delete previous images
-        var prev = document.querySelector('.container img');
-        if (prev) {
-          prev.parentNode.removeChild(prev);
-
-        }
-        var polylines = document.querySelectorAll('#svg2 polyline');
-        if (polylines.length) {
-          for (var i = 0; i < polylines.length; i++) {
-            polylines[i].parentNode.removeChild(polylines[i]);
-          }
-        }
-
-        var images = document.querySelectorAll('#svg2 image');
-
-        if (images.length) {
-          for (var i = 0; i < images.length; i++) {
-            images[i].parentNode.removeChild(images[i]);
-          }
-        }
-        gimage.style.opacity = 1;
-        imageWidth = gimage.width;
-
-        dismsg.innerHTML = "Processing...";
-        document.querySelector('.container')
-          .appendChild(gimage);
-        canvas.loadImg(gimage.src, 0, 0, resultWidth, resultHeight).then(process);
-
-      }
-    }
-
-  }
-
-  function importCanvas(sourceCanvas, targetSVG) {
-    var img_dataurl = sourceCanvas.toDataURL("image/png");
-
-    var svg_img = document.createElementNS(
-      "http://www.w3.org/2000/svg", "image");
-
-    svg_img.setAttributeNS(
-      "http://www.w3.org/1999/xlink", "xlink:href", img_dataurl);
-
-    targetSVG.appendChild(svg_img);
-  }
-
-  var BezierCurveBrush = {
+ var BezierCurveBrush = {
     // inner variables
     iPrevX: 0,
     iPrevY: 0,
@@ -211,8 +83,9 @@ $(document).ready(function () {
           var iSecondPoint = this.getPoint(parseInt(settings.SketchStrength / 3), this.points);
           ctx.moveTo(iStartPoint[0], iStartPoint[1]);
           //ctx.shadowBlur = parseInt(Math.random() * (8 - 1) + 1);
+         // ctx.shadowBlur = 3;
           //console.log(ctx.shadowBlur);
-          //ctx.shadowColor = 'rgba(0, 0, 0,0.5)';
+          //ctx.shadowColor = 'rgba(0, 0, 0,0.8)';
           ctx.bezierCurveTo(iFirstPoint[0], iFirstPoint[1], iSecondPoint[0], iSecondPoint[1], x, y);
           ctx.stroke();
           ctx.closePath();
@@ -222,31 +95,91 @@ $(document).ready(function () {
 
       }
     }
-  };
+  }; 
+function importCanvas(sourceCanvas, targetSVG) {
+    var img_dataurl = sourceCanvas.toDataURL("image/png");
 
-  var dropAreaElement = document.querySelector('.main');
+    var svg_img = document.createElementNS(
+      "http://www.w3.org/2000/svg", "image");
 
-  var imageProvider = new ImageProvider({
-    element: dropAreaElement,
-    onImageRead: function (image) {
-      gimage = image;
-      sketchi();
+    svg_img.setAttributeNS(
+      "http://www.w3.org/1999/xlink", "xlink:href", img_dataurl);
 
+    targetSVG.appendChild(svg_img);
+}
+ function sketchi() {
+    
+    $("#stat").fadeIn();
+    $("#stat").html("Loading photo...");
+    var ctxt;
+    var imageTmp = new Image();
+    imageTmp.id = 'result';
+    imageTmp.src = gimage.src;
+    imageTmp.onload = function () {
 
-      dropAreaElement.classList.add('dropped');
-      contourFinder = new ContourFinder();
-      console.log("first");
-      canvas = new Canvas('canvas', resultWidth, resultHeight);
+      var aspectRatio = imageTmp.width / imageTmp.height;
+      console.log("w x h " + imageTmp.width + " " + imageTmp.height);
+      console.log("AR :" + aspectRatio);
+      if (imageTmp.width > 640) {
+        resultWidth = 640;
+        resultHeight = 640 / aspectRatio;
+      }
+      else {
+        resultWidth = imageTmp.width;
+        resultHeight = imageTmp.height;
 
-      canny = new Canny(canvas);
-      filters = new Filters(canvas);
+      }
 
+      console.log("w x h" + resultWidth + " " + resultHeight);
+      
+      scanvas = document.createElement('canvas');
+      scanvas.id = 'canvas2';
+      scanvas.width = resultWidth;
+      scanvas.height = resultHeight;
+      ctxt = scanvas.getContext("2d");
+      ctxt.drawImage(imageTmp, 0, 0, resultWidth, resultHeight);
+      gimage.src = scanvas.toDataURL();
+      //delete imageTmp;
+      
+     
+      gimage.onload = function () {
+        // delete previous images
+        var prev = document.querySelector('.container img');
+        if (prev) {
+          prev.parentNode.removeChild(prev);
 
+        }
+        var polylines = document.querySelectorAll('#svg2 polyline');
+        if (polylines.length) {
+          for (var i = 0; i < polylines.length; i++) {
+            polylines[i].parentNode.removeChild(polylines[i]);
+          }
+        }
 
+        var images = document.querySelectorAll('#svg2 image');
+
+        if (images.length) {
+          for (var i = 0; i < images.length; i++) {
+            images[i].parentNode.removeChild(images[i]);
+          }
+        }
+        gimage.style.opacity = 1;
+        imageWidth = gimage.width;
+         $("#stat").html("Processing...");
+        
+        document.querySelector('.container')
+          .appendChild(gimage);
+        contourFinder = new ContourFinder();
+     
+         canvas = new Canvas('canvas', resultWidth, resultHeight);
+         canny = new Canny(canvas);
+         filters = new Filters(canvas);  
+         canvas.loadImg(gimage.src, 0, 0, resultWidth, resultHeight).then(process);
+
+      }
     }
-  });
 
-  imageProvider.init();
+  }
 
   function process() {
 
@@ -257,63 +190,32 @@ $(document).ready(function () {
     canvas.setImgData(filters.grayscale());
     dismsg.innerHTML = "Processing...10%";
     NProgress.set(0.2);
-    
+
     //find odd num odd=a+(n-1)d;
     // odd=1+(n-1)*2
-
     var sm=1+((settings.Smoothness-1)*2);
-    console.log(sm);
+
     canvas.setImgData(filters.gaussianBlur(5,sm));
-    dismsg.innerHTML = "Processing...40%";
+    $("#stat").html( "Processing...40%");
     NProgress.set(0.3);
     canvas.setImgData(canny.gradient('sobel'));
-    dismsg.innerHTML = "Processing...50%";
+   $("#stat").html("Processing...50%");
     NProgress.set(0.4);
     canvas.setImgData(canny.nonMaximumSuppress());
-    dismsg.innerHTML = "Processing...60%";
+    $("#stat").html("Processing...60%");
     NProgress.set(0.5);
     canvas.setImgData(canny.hysteresis());
-    dismsg.innerHTML = "Processing...80%";
-    console.log(dismsg.innerHTML);
+    $("#stat").html( "Processing...80%");
     NProgress.set(0.55);
     contourFinder.init(canvas.getCanvas());
     contourFinder.findContours();
-    dismsg.innerHTML = "Sketching...";
+    $("#stat").html("Sketching...");
     NProgress.set(0.6);
     
     drawContours();
   
   }
-
-  function findOutDirection(point1, point2) {
-    if (point2.x > point1.x) {
-      if (point2.y > point1.y) {
-        return DIRECTIONS.NE;
-      } else if (point2.y < point1.y) {
-        return DIRECTIONS.SE;
-      } else {
-        return DIRECTIONS.E;
-      }
-    } else if (point2.x < point1.x) {
-      if (point2.y > point1.y) {
-        return DIRECTIONS.NW;
-      } else if (point2.y < point1.y) {
-        return DIRECTIONS.SW;
-      } else {
-        return DIRECTIONS.W;
-      }
-    } else {
-      if (point2.y > point1.y) {
-        return DIRECTIONS.N;
-      } else if (point2.y < point1.y) {
-        return DIRECTIONS.S;
-      } else {
-        return DIRECTIONS.SAME;
-      }
-    }
-  }
-
-  function drawContours() {
+function drawContours() {
 
     ctx = scanvas.getContext("2d");
     ctx.canvas.height = resultWidth;
@@ -332,10 +234,8 @@ $(document).ready(function () {
 
     animate();
   }
-  function draw() {
 
-  }
-  function drawContour(index) {
+function drawContour(index) {
     var points = contourFinder.allContours[index];
 
     var optimizedPoints = [],
@@ -434,6 +334,110 @@ $(document).ready(function () {
       NProgress.done();
     }, 2000);
   }
+
+   function findOutDirection(point1, point2) {
+    if (point2.x > point1.x) {
+      if (point2.y > point1.y) {
+        return DIRECTIONS.NE;
+      } else if (point2.y < point1.y) {
+        return DIRECTIONS.SE;
+      } else {
+        return DIRECTIONS.E;
+      }
+    } else if (point2.x < point1.x) {
+      if (point2.y > point1.y) {
+        return DIRECTIONS.NW;
+      } else if (point2.y < point1.y) {
+        return DIRECTIONS.SW;
+      } else {
+        return DIRECTIONS.W;
+      }
+    } else {
+      if (point2.y > point1.y) {
+        return DIRECTIONS.N;
+      } else if (point2.y < point1.y) {
+        return DIRECTIONS.S;
+      } else {
+        return DIRECTIONS.SAME;
+      }
+    }
+  }
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+}
+
+$(document).ready(function () {
+ 
+  NProgress.configure({ showSpinner: false });
+  NProgress.configure({ trickle: false });
+  dismsg = document.getElementById("stat");
+
+  var gui = new dat.gui.GUI();
+  gui.domElement.id = 'gui';
+ 
+  gui.add(settings, 'Style', ['sketch', 'simple', 'chalkboard']).onChange(function (style) {
+    
+    if (style == 'chalkboard') {
+      settings.BackgroundColor = [50, 50, 50];
+      settings.BrushColor = [200, 200, 200];
+    }
+    else {
+      settings.BackgroundColor = [255, 255, 255];
+      settings.BrushColor = [50, 50, 50];
+
+    }
+
+  });
+
+  gui.add(settings, 'BrushSize').min(0).max(5).step(1);
+  gui.add(settings, 'Smoothness').min(1).max(5).step(1);
+  gui.add(settings, 'SketchStrength').min(1).max(100).step(1);
+  
+  var f1 = gui.addFolder('Colors');
+  f1.addColor(settings, 'BrushColor').listen();;
+  f1.addColor(settings, 'BackgroundColor').listen();;
+
+  function downloadCanvas(link, sourceCanvas, filename) {
+    link.href = sourceCanvas.toDataURL();
+    link.download = filename;
+  }
+
+  document.getElementById('download').addEventListener('click', function () {
+    downloadCanvas(this, scanvas, 'sketch.png');
+  }, false);
+  
+  document.getElementById('redo').addEventListener('click', function () {
+    sketchi();
+  }, false);
+
+  var dropAreaElement = document.querySelector('.main');
+  var imageProvider = new ImageProvider({
+    element: dropAreaElement,
+    
+    onImageRead: function (image) {
+      gimage = image;
+      sketchi();
+
+    }
+  });
+  imageProvider.init();
 
 });
 
